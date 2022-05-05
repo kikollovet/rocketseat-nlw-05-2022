1e95e42dd7bbf5b27fa4364ddd9b3b193295b5f7
Kiko <kikollovet@gmail.com>
2022-05-05 12:16:32 -0300

    Coração no footer do Widget

--- src/components/WidgetForm/index.tsx
+++ src/components/WidgetForm/index.tsx
@@ -64,7 +64,7 @@ export function WidgetForm() {
             )}
 
             <footer className="text-xs text-neutral-400">
-                Feito com S2 pela <a className="underline underline-offset-2" href="http://rocketseat.com.br">Rocketseat</a>
+                Feito com ♥ pela <a className="underline underline-offset-2" href="http://rocketseat.com.br">Rocketseat</a>
             </footer>
         </div>
     )
